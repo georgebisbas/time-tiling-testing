d00dc80c242ca00f6b21211deb9efbac764446a2
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-04-16 19:31:44 +0100

    Identation and cleanup

--- BUFFER_time_tiling.c
+++ BUFFER_time_tiling.c
@@ -12,177 +12,180 @@
 #include "edit_buffer.h"
 //#define min(a, b) (((a) < (b)) ? (a) : (b))
 
-int main(int argc, char **argv) {
-    //int nrows = atoi(argv[1]);  //Number of rows
-    //int ncols = atoi(argv[2]);  //Number of columns
-    //int timesteps = atoi(argv[3]); //Number of timsteps
-    int tile_size = atoi(argv[1]); // Size of tile (space and time)
-    int num_threads = atoi(argv[2]); // Size of tile (space and time)
+int main(int argc, char **argv)
+{
+  //int nrows = atoi(argv[1]);  //Number of rows
+  //int ncols = atoi(argv[2]);  //Number of columns
+  //int timesteps = atoi(argv[3]); //Number of timsteps
+  int tile_size = atoi(argv[1]);   // Size of tile (space and time)
+  int num_threads = atoi(argv[2]); // Size of tile (space and time)
+  
+  // Print a segment of the calculations and define starting and ending indexes
+  int print_results = 0; 
+  int si = 10;
+  int ei = 15;
+  int sc = 10;
+  int ec = 15;
 
-    int print_results = 0;    // Print a segment of the calculations
-    int si = 10; int ei = 15; int sc = 10; int ec = 15; // Bounds of the segment
+  //Define sources and receivers numbers
+  int nsrc = 1;   // Number of sources
+  int nrecs = 10; // Number of receivers
+  int omp_opt = 0; // Option for openMP . (NOT USED) TODO
 
-    int nsrc = 1;  // Number of sources
-    int nrecs = 10; // Number of receivers
-    //int timestamps = timesteps;     // To be removed after implementing buffering
-    //int timestamps2 = timesteps;    // To be removed after implementing buffering
-    int omp_opt=0;                  // Option for openMP . NOT USED
+  // Time measurement
+  struct timeval t1, t2;
+  double elapsedTime1, elapsedTime2;
+  double sum_elapsedTime1, sum_elapsedTime2;
 
-    // Time measurement
-    struct timeval t1, t2;
-    double elapsedTime1, elapsedTime2;
-		double sum_elapsedTime1, sum_elapsedTime2;
+  omp_set_num_threads(num_threads);
+  /* Flush denormal numbers to zero in hardware */
+  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 
-    omp_set_num_threads(num_threads);
-    /* Flush denormal numbers to zero in hardware */
-    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
-    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+  //printf("Problem setup is \nnrows: %d\nncols: %d\nTimesteps: %d\nTimestamps: %d\n",nrows, ncols, timesteps, timestamps);
+  
+  //Define a 2d matrix to store the results
+  double **RESULTS;
+  malloc2d(&RESULTS, 40, 10);
 
-    //printf("Problem setup is \nnrows: %d\nncols: %d\nTimesteps: %d\nTimestamps: %d\n",nrows, ncols, timesteps, timestamps);
+  int validation_iters = 10; //Number of iterations
+  int ri = -1;
+  int rj = 0;
+  int rs = 13; 
+  int re = 14;
+  int ts = 6;
+  int te = 9;
 
-    //double ** A;
-    //malloc2d(&A, nrows, ncols);
-    //double ** B; // B is like A(t-1)
-    //malloc2d(&B, nrows, ncols);
-    //double ** src_coords; // B is like A(t-1)
-    //malloc2d(&src_coords, nsrc, 2);
-    //double ** rec_coords; // B is like A(t-1)
-    //malloc2d(&rec_coords, nsrc, 2);
-    //double ** src; // B is like A(t-1)
-    //malloc2d(&src, nsrc, 2);
+  for (int rows_pow = rs; rows_pow < re; rows_pow++)
+  {
+    for (int time_pow = ts; time_pow < te; time_pow++)
+    {
+      int nrows = pow(2, rows_pow);
+      int ncols = pow(2, rows_pow);
+      int timesteps = pow(2, time_pow);
 
-		double ** RESULTS;
-    malloc2d(&RESULTS, 40, 10);
+      int timestamps = 2;  // To be removed after implementing buffering
 
-		int validation_iters = 10;
-		int ri=-1; int rj=0;
-		for(int rows_pow = 11 ; rows_pow < 14; rows_pow++){
-			//for(int cols_pow = 5 ; cols_pow< 11; cols_pow++){
-//ri++;
-			for(int time_pow = 5 ; time_pow < 9; time_pow++){
-        int nrows = pow( 2 ,  rows_pow);
-  			int ncols = pow( 2 ,  rows_pow);
-  			int timesteps = pow( 2 ,  time_pow);
+      if (pow(2, 2 * rows_pow) * 2 < pow(2, 31))
+      {
+        ri++;
 
-  			int timestamps = 2;     // To be removed after implementing buffering
-  			int timestamps2 = 2;    // To be removed after implementing buffering
+        RESULTS[ri][0] = nrows;
+        RESULTS[ri][1] = timesteps;
+        RESULTS[ri][2] = tile_size;
 
+        printf("Problem setup is \nnrows: %d\nncols: %d\nTimesteps: %d\nTimestamps: %d\n", nrows, ncols, timesteps, timestamps);
 
-      if (pow( 2 ,  2*rows_pow)*2 < pow( 2 ,  31) ){
-      ri++;
+        //printf("Allocated\n");
 
+        sum_elapsedTime1 = 0;
+        sum_elapsedTime2 = 0;
+        double ***u;
+        u = createMatrix(timestamps, nrows, ncols);
+        double ***u2;
+        u2 = createMatrix(timestamps, nrows, ncols);
 
-			RESULTS[ri][0] = nrows;
-			RESULTS[ri][1] = timesteps;
-			RESULTS[ri][2] = tile_size;
+        for (int validation_index = 0; validation_index < validation_iters; validation_index++)
+        {
+          // Set initial values
+          initialize3(timestamps, nrows, ncols, u);
+          initialize3(timestamps, nrows, ncols, u2);
 
-		printf("Problem setup is \nnrows: %d\nncols: %d\nTimesteps: %d\nTimestamps: %d\n", nrows, ncols, timesteps, timestamps);
+          //printf("\n Starting Jacobi...");
+          gettimeofday(&t1, NULL);
+          u = jacobi_3d(timesteps, nrows, ncols, u, omp_opt = 0, tile_size);
+          gettimeofday(&t2, NULL);
+          //printf("... Finished \n");
+          elapsedTime1 = (double)(t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec) / 1000000;
+          //printf("Jacobi OpenMP, Time taken by program is : %3.3f\n",elapsedTime1);
 
+          //printf("Starting Jacobi...");
+          gettimeofday(&t1, NULL);
+          u2 = tiled_skewed_buffered(timesteps, nrows, ncols, u2, omp_opt = 0, tile_size);
+          gettimeofday(&t2, NULL);
+          //printf("... Finished \n");
+          elapsedTime2 = (double)(t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec) / 1000000;
+          //printf("Jacobi skewed, Time taken by program is : %3.3f\n",elapsedTime2);
 
+          printf(" Speedup from skeweing is : %3.3f\n", elapsedTime1 / elapsedTime2);
+          sum_elapsedTime1 += elapsedTime1;
+          sum_elapsedTime2 += elapsedTime2;
 
-    //printf("Allocated\n");
-
-
-		sum_elapsedTime1 = 0 ;
-		sum_elapsedTime2 = 0;
-    double *** u;
-    u = createMatrix(timestamps, nrows, ncols);
-    double *** u2;
-    u2 = createMatrix(timestamps2, nrows, ncols);
-
-
-for(int validation_index=0; validation_index<validation_iters;validation_index++){
-
-    initialize3( timestamps, nrows, ncols, u);
-    initialize3( timestamps2,nrows, ncols, u2);
-
-    //printf("\n Starting Jacobi...");
-		gettimeofday(&t1, NULL);
-    u = jacobi_3d(timesteps, nrows, ncols, u, omp_opt=0, tile_size);
-    gettimeofday(&t2, NULL);
-    //printf("... Finished \n");
-    elapsedTime1 = (double)(t2.tv_sec-t1.tv_sec)+(double)(t2.tv_usec-t1.tv_usec)/1000000;
-    //printf("Jacobi OpenMP, Time taken by program is : %3.3f\n",elapsedTime1);
-
-    //printf("Starting Jacobi...");
-		gettimeofday(&t1, NULL);
-    u2 = tiled_skewed_buffered(timesteps, nrows, ncols, u2, omp_opt=0,tile_size);
-    gettimeofday(&t2, NULL);
-    //printf("... Finished \n");
-    elapsedTime2 = (double)(t2.tv_sec-t1.tv_sec)+(double)(t2.tv_usec-t1.tv_usec)/1000000;
-    //printf("Jacobi skewed, Time taken by program is : %3.3f\n",elapsedTime2);
-
-    printf(" Speedup from skeweing is : %3.3f\n", elapsedTime1/elapsedTime2);
-		sum_elapsedTime1 += elapsedTime1;
-		sum_elapsedTime2 += elapsedTime2;
-
-    int validate_flag=1;
-    if(validate_flag){
-        //for (int k = 0; k < timestamps; k++) {
-            for (int i = 0; i < nrows; i++) {
-              for (int j = 0; j < ncols; j++) {
-              if ((u[1][i][j] - u2[1][i][j])> 0.00001) {
-                printf(" Failed %d, %d %f \n",i, j,(u[1][i][j] - u2[1][i][j]));
+          int validate_flag = 1;
+          if (validate_flag)
+          {
+            for (int i = 0; i < nrows; i++)
+            {
+              for (int j = 0; j < ncols; j++)
+              {
+                if ((u[1][i][j] - u2[1][i][j]) > 0.00001)
+                {
+                  printf(" Failed %d, %d %f \n", i, j, (u[1][i][j] - u2[1][i][j]));
+                }
               }
             }
           }
-        //}
-    }
+          if (print_results)
+          {
+            printf("\n ------------------------\n");
+            for (int i = si; i < ei; i++)
+            {
+              printf("\n");
+              for (int j = sc; j < ec; j++)
+              {
+                printf(" %3.3f", u[1][i][j]);
+              }
+            }
+          }
 
-    if(print_results){
-      printf("\n ------------------------\n");
-        for (int i = si; i < ei; i++) {
-          printf("\n");
-          for (int j = sc; j < ec; j++) {
-          printf(" %3.3f", u[1][i][j]);
+          if (print_results)
+          {
+            printf("\n ------------------------\n");
+            for (int i = si; i < ei; i++)
+            {
+              printf("\n");
+              for (int j = sc; j < ec; j++)
+              {
+                printf(" %3.3f", u2[1][i][j]);
+              }
+            }
+          }
         }
-      }
-    }
 
-    if(print_results){
-      printf("\n ------------------------\n");
-        for (int i = si; i < ei; i++) {
-          printf("\n");
-          for (int j = sc; j < ec; j++) {
-          printf(" %3.3f", u2[1][i][j]);
-        }
-      }
-    }
+        RESULTS[ri][3] = sum_elapsedTime1 / validation_iters;
+        RESULTS[ri][4] = sum_elapsedTime2 / validation_iters;
+        RESULTS[ri][5] = sum_elapsedTime1 / sum_elapsedTime2;
 
-}
+        printf("Problem setup is \nnrows: %d\nncols: %d\nTimesteps: %d\nTimestamps: %d\n", nrows, ncols, timesteps, timestamps);
+        printf(" Average speedup from skeweing is : %3.3f\n", sum_elapsedTime1 / sum_elapsedTime2);
 
-RESULTS[ri][3] = sum_elapsedTime1/validation_iters;
-RESULTS[ri][4] = sum_elapsedTime2/validation_iters;
-RESULTS[ri][5] = sum_elapsedTime1/sum_elapsedTime2;
+        free(u);
+        free(u2);
 
-printf("Problem setup is \nnrows: %d\nncols: %d\nTimesteps: %d\nTimestamps: %d\n",nrows, ncols, timesteps, timestamps);
-printf(" Average speedup from skeweing is : %3.3f\n", sum_elapsedTime1/sum_elapsedTime2);
+        printf("\n");
 
-free(u);
-free(u2);
+      } //rows_pow
+    } //if
+  }   //time_pow
 
-
-    printf("\n");
-
-	}//rows_pow
-//}//cols_pow
-}//if
-}//time_pow
-
-if(1){
-  printf("\n ------------------------\n");
-    for (int i = 0; i < 20; i++) {
+  if (1)
+  {
+    printf("\n ------------------------\n");
+    for (int i = 0; i < 20; i++)
+    {
       printf("\n");
-      for (int j = 0; j < 6; j++) {
-      printf(" %3.3f", RESULTS[i][j]);
+      for (int j = 0; j < 6; j++)
+      {
+        printf(" %3.3f", RESULTS[i][j]);
+      }
     }
   }
-}
 
-char str[100] = "Results";
-//printf("\n Enter the filename :");
-//gets(str);
+  char str[100] = "Results";
+  // Uncomment to edit the name of the file.
+  // printf("\n Enter the filename :"); gets(str);
+  
 
-create_results_csv(str, RESULTS, 6, 20);
-    return 0;
+  create_results_csv(str, RESULTS, 6, 20);
+  return 0;
 }
--- edit_buffer.h
+++ edit_buffer.h
@@ -1,78 +1,95 @@
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-void create_results_csv(char *filename, double **a,int n,int m){
-printf("\n Creating %s.csv file",filename);
-FILE *fp;
-int i,j;
-filename=strcat(filename,".csv");
-fp=fopen(filename,"w+");
-fprintf(fp,"NROWS, NCOLS, Timesteps, Tilesize, el1, el2, speedup");
-for(i=0;i<m;i++){
-    fprintf(fp,"\n%d",i+1);
-    for(j=0;j<n;j++)
-        fprintf(fp,", %f ",a[i][j]);
-    }
-fclose(fp);
-printf("\n %sfile created",filename);
+//Function to store results in a csv file
+void create_results_csv(char *filename, double **a, int n, int m)
+{
+  printf("\n Creating %s.csv file", filename);
+  FILE *fp;
+  int i, j;
+  filename = strcat(filename, ".csv");
+  fp = fopen(filename, "w+");
+  fprintf(fp, "NROWS, NCOLS, Timesteps, Tilesize, el1, el2, speedup");
+  for (i = 0; i < m; i++)
+  {
+    fprintf(fp, "\n%d", i + 1);
+    for (j = 0; j < n; j++)
+      fprintf(fp, ", %f ", a[i][j]);
+  }
+  fclose(fp);
+  printf("\n %sfile created", filename);
 }
 
-// Function to allocate memory for u[x][y][t]
-double*** createMatrix( int timestamps, int nrows, int ncols) {
-//  printf("\nAllocating 3d...");
+// Function to allocate memory for u[t][x][y]
+double ***createMatrix(int timestamps, int nrows, int ncols)
+{
+  //  printf("\nAllocating 3d...");
   double ***matrix;
 
-  matrix = (double***)malloc(timestamps*sizeof(double**));
-  for (int nt = 0; nt < timestamps; nt++) {
-    matrix[nt] = (double**)malloc(nrows*sizeof(double*));
-    for (int row = 0; row < nrows; row++) {
-      matrix[nt][row] = (double*)malloc(ncols*sizeof(double));
+  matrix = (double ***)malloc(timestamps * sizeof(double **));
+  for (int nt = 0; nt < timestamps; nt++)
+  {
+    matrix[nt] = (double **)malloc(nrows * sizeof(double *));
+    for (int row = 0; row < nrows; row++)
+    {
+      matrix[nt][row] = (double *)malloc(ncols * sizeof(double));
     }
   }
   //printf("...DONE\n");
   return matrix;
 }
 
-void initialize3( int timestamps, int nrows, int ncols, double ***A_init)
+// Function to allocate memory for u[t][x][y]
+void initialize3(int timestamps, int nrows, int ncols, double ***A_init)
 {
-    // Initialize the matrix
-    //printf("\nInit 3d...");
-    int ti = 0;
-    int xi = 0;
-    int yi = 0;
-    for (ti = 0; ti < timestamps; ti+=1) {
-      for (xi = 0; xi < nrows; xi+=1) {
-        for (yi = 0; yi < ncols; yi+=1) {
-          A_init[ti][xi][yi] = 2*xi-yi+fabs(xi-yi);
-        }
+  // Initialize the matrix
+  //printf("\nInit 3d...");
+  int ti = 0;
+  int xi = 0;
+  int yi = 0;
+  for (ti = 0; ti < timestamps; ti += 1)
+  {
+    for (xi = 0; xi < nrows; xi += 1)
+    {
+      for (yi = 0; yi < ncols; yi += 1)
+      {
+        A_init[ti][xi][yi] = 2 * xi - 1.4 * yi + 0.5 * fabs(xi - yi);
+      }
     }
   }
   //printf("...DONE\n");
 }
 
-double malloc2d(double *** C, int nrows, int ncols) {
-    int i;
-    *C = malloc( sizeof(double *) * nrows);
+// Function to malloc a 2d array
+double malloc2d(double ***C, int nrows, int ncols)
+{
+  int i;
+  *C = malloc(sizeof(double *) * nrows);
 
-    if (*C == NULL) {
-        printf("ERROR: out of memory\n");
-        return 1;
-    }
+  if (*C == NULL)
+  {
+    printf("ERROR: out of memory\n");
+    return 1;
+  }
 
-    for (i=0; i<nrows; i++) {
-        (*C)[i] = malloc( sizeof(double) * ncols);
-        if ((*C)[i] == NULL) {
-            printf("ERROR: out of memory\n");
-            return 1;
-        }
+  for (i = 0; i < nrows; i++)
+  {
+    (*C)[i] = malloc(sizeof(double) * ncols);
+    if ((*C)[i] == NULL)
+    {
+      printf("ERROR: out of memory\n");
+      return 1;
     }
-    //printf("Allocated!\n");
-    return 0;
+  }
+  //printf("Allocated!\n");
+  return 0;
 }
 
-double ** * jacobi_3d(int timesteps, int nrows, int ncols, double ** * grid, int omp_opt, int tile_size) {
+// Function to malloc a 2d array
+double ***jacobi_3d(int timesteps, int nrows, int ncols, double ***grid, int omp_opt, int tile_size)
+{
   int t = 0;
   int xi = 0;
   int yi = 0;
@@ -83,27 +100,31 @@ double ** * jacobi_3d(int timesteps, int nrows, int ncols, double ** * grid, int
   int y_m = 1;
   int y_M = ncols - 1;
 
-  for (int titer = 0; titer < timesteps-1; titer++) {
-    t1 = titer%2 ;
+  for (int titer = 0; titer < timesteps - 1; titer++)
+  {
+    t1 = titer % 2;
     t0 = !t1;
 
 #pragma omp parallel for
-  for (int xi = x_m; xi < x_M; xi++) {
-    #pragma omp simd
-        for (int yi = y_m; yi < y_M; yi++) {
+    for (int xi = x_m; xi < x_M; xi++)
+    {
+#pragma omp simd
+      for (int yi = y_m; yi < y_M; yi++)
+      {
         grid[t1][xi][yi] = (grid[t0][xi][yi] + grid[t0][xi - 1][yi] + grid[t0][xi + 1][yi] + grid[t0][xi][yi - 1] + grid[t0][xi][yi + 1]) / 5;
-        }
       }
+    }
   }
   return grid;
-
 }
 
-double ** * tiled_skewed_jacobi_3d(int timesteps, int nrows, int ncols, double ** * grid, int omp_opt, int tile_size) {
+double ***tiled_skewed_jacobi_3d(int timesteps, int nrows, int ncols, double ***grid, int omp_opt, int tile_size)
+{
   int t = 0;
   int xi = 0;
   int yi = 0;
-  int t1=1; int t0=0;
+  int t1 = 1;
+  int t0 = 0;
 
   int x_m = 1;
   int x_M = nrows - 1;
@@ -113,35 +134,38 @@ double ** * tiled_skewed_jacobi_3d(int timesteps, int nrows, int ncols, double *
   int t_blk_size = tile_size;
   int x_blk_size = tile_size;
 
-// x blocking
-    for (int xblk = x_m; xblk < x_M + timesteps-1; xblk+= x_blk_size) {
-// t interchange
-      for (int titer = 0; titer < timesteps-1; titer++) {
+  // x blocking
+  for (int xblk = x_m; xblk < x_M + timesteps - 1; xblk += x_blk_size)
+  {
+    // t interchange
+    for (int titer = 0; titer < timesteps - 1; titer++)
+    {
 
-        t1 = titer%2 ;
-        t0 = !t1;
-    // parallelize for x blocks
-    #pragma omp parallel for
-     for (int xi = max(x_m + titer, xblk); xi < min( (x_M + titer), (xblk + x_blk_size)); xi++) {
-// simd
-    #pragma omp simd
-    for (int yi = y_m + titer; yi < y_M + titer; yi++) {
-  grid[t1][xi - titer][yi - titer] = (grid[t0][xi - titer][yi - titer] + grid[t0][(xi - titer) - 1][yi - titer] + grid[t0][(xi - titer) + 1][yi - titer] + grid[t0][xi - titer][yi - titer - 1] + grid[t0][xi - titer][yi - titer + 1]) / 5;
+      t1 = titer % 2;
+      t0 = !t1;
+// parallelize for x blocks
+#pragma omp parallel for
+      for (int xi = max(x_m + titer, xblk); xi < min((x_M + titer), (xblk + x_blk_size)); xi++)
+      {
+        // simd
+#pragma omp simd
+        for (int yi = y_m + titer; yi < y_M + titer; yi++)
+        {
+          grid[t1][xi - titer][yi - titer] = (grid[t0][xi - titer][yi - titer] + grid[t0][(xi - titer) - 1][yi - titer] + grid[t0][(xi - titer) + 1][yi - titer] + grid[t0][xi - titer][yi - titer - 1] + grid[t0][xi - titer][yi - titer + 1]) / 5;
         }
       }
+    }
   }
-
-}
   return grid;
 }
 
-
-
-double ** * tiled_skewed_buffered(int timesteps, int nrows, int ncols, double ** * grid, int omp_opt, int tile_size) {
+double ***tiled_skewed_buffered(int timesteps, int nrows, int ncols, double ***grid, int omp_opt, int tile_size)
+{
   int t = 0;
   int xi = 0;
   int yi = 0;
-  int t1=1; int t0=0;
+  int t1 = 1;
+  int t0 = 0;
 
   int x_m = 1;
   int x_M = nrows - 1;
@@ -151,24 +175,26 @@ double ** * tiled_skewed_buffered(int timesteps, int nrows, int ncols, double **
   int t_blk_size = tile_size;
   int x_blk_size = tile_size;
 
-// x blocking
-    for (int xblk = x_m; xblk < x_M + timesteps-1; xblk+= x_blk_size) {
-// t interchange
-      for (int titer = 0; titer < timesteps-1; titer++) {
-
-    t1 = titer%2 ;
-    t0 = !t1;
-    // parallelize for x blocks
-    #pragma omp parallel for
-     for (int xi = max(x_m + titer, xblk); xi < min( (x_M + titer), (xblk + x_blk_size)); xi++) {
-// simd
-    #pragma omp simd
-    for (int yi = y_m + titer; yi < y_M + titer; yi++) {
-  grid[t1][xi - titer][yi - titer] = (grid[t0][xi - titer][yi - titer] + grid[t0][(xi - titer) - 1][yi - titer] + grid[t0][(xi - titer) + 1][yi - titer] + grid[t0][xi - titer][yi - titer - 1] + grid[t0][xi - titer][yi - titer + 1]) / 5;
+  // x blocking
+  for (int xblk = x_m; xblk < x_M + timesteps - 1; xblk += x_blk_size)
+  {
+    // t interchange
+    for (int titer = 0; titer < timesteps - 1; titer++)
+    {
+      t1 = titer % 2;
+      t0 = !t1;
+// parallelize for x blocks
+#pragma omp parallel for
+      for (int xi = max(x_m + titer, xblk); xi < min((x_M + titer), (xblk + x_blk_size)); xi++)
+      {
+        // simd
+#pragma omp simd
+        for (int yi = y_m + titer; yi < y_M + titer; yi++)
+        {
+          grid[t1][xi - titer][yi - titer] = (grid[t0][xi - titer][yi - titer] + grid[t0][(xi - titer) - 1][yi - titer] + grid[t0][(xi - titer) + 1][yi - titer] + grid[t0][xi - titer][yi - titer - 1] + grid[t0][xi - titer][yi - titer + 1]) / 5;
         }
       }
+    }
   }
-
-}
   return grid;
 }
